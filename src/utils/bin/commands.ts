7e922c1b1f246a6477d811706896d3b5b8c1db40
Hylierr <80465987+Hylierr@users.noreply.github.com>
2024-12-20 19:33:17 +0300

    Update commands.ts

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -139,14 +139,14 @@ export const sudo = async (args?: string[]): Promise<string> => {
 
 // Banner
 export const banner = (args?: string[]): string => {
-  return `                                                              
+  return `
                               _____                                 
     __   _ __    _ ____   ___|__   |__  ______  _____   ____    __  
    |  |_| |\ \  //|    | |_    _|     ||   ___||     | |    \  /  | 
    |   _  | \ \// |    |_ |    |      ||   ___||     \ |     \/   | 
    |__| |_| /__/  |______||____|    __||______||__|\__\|__/\__/|__| 
                              |_____|                                
-                                                                  
+
 Type 'help' to see the list of available commands.
 Type 'sumfetch' to display summary.
 Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
